5fee222b3d65d275664545004090157163fa90fe
Yashweta <yash.211128@srttc.ac.in>
2024-03-14 12:16:27 +0530

    updated

--- library/urls.py
+++ library/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',views. CustomLoginView.as_view(), name='login'),
@@ -8,4 +9,5 @@ urlpatterns = [
     path('view_issued_books/', views.view_issued_books, name='view_issued_books'),
     path('issue_book/', views.issue_book, name='issue_book'),
     path('logout/', views.custom_logout, name='logout'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
 ]
